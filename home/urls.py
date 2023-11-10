d408dd5d51634fee8bb638207d3a394128ac3884
Carlos Wilson <carloswilsonperez@gmail.com>
2023-11-09 23:41:49 -0600

    Updating settings

--- home/urls.py
+++ home/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 from  .views import (
-    home, about, gallery, contact, family, pictures2004, pictures2005
+    home, family
 )
 
 urlpatterns = [
     path("", home, name="home"),
-    path("about/", about, name="about"),
-    path("gallery/", gallery, name="gallery"),
-    path("/", family, name="family"),
-    path("contact/", contact, name="contact"),
-    path("pictures2004/", pictures2004, name="pictures2004"),
-    path("pictures2005/", pictures2005, name="pictures2005")
+    path("gallery/", family, name="family"),
 ]
